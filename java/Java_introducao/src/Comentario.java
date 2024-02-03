28b50c387b69238bcb779d80620f6e1482ecc4cd
andre <andrinho@gmail.com>
2024-02-03 01:14:16 -0300

    first commit

---
+++ java/Java_introducao/src/Comentario.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *  JAVADOC  -  comentario voltado para documentacao
+ *
+ */
+
+public class Comentario {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java/Java_introducao/src/Variaveis.java
@@ -0,0 +1,25 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        String nome = "Andre";
+        System.out.println("minimo short: "+ Short.MIN_VALUE + " e maixmo short: "+Short.MAX_VALUE);
+        System.out.println("minimo Integer: "+ Integer.MIN_VALUE + " e maximo Integer: "+Integer.MAX_VALUE);
+        System.out.println("minimo Long: "+ Long.MIN_VALUE + " e maximo Long: "+Long.MAX_VALUE);
+
+    }
+}
+/**
+ * Tipos de dados
+ *
+ * Double 64
+ * FLoat 32
+ *
+ * Long 64
+ * Integer 32
+ * Short 16
+ * Byte 8
+ *
+ *Boolean ?
+ * String ?
+ * Character ?
+ *
+ * */
